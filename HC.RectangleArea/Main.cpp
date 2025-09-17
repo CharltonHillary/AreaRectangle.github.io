c067bb6db161a3c84ad8790ebb26dd9f9943bd98
Charlton, Hillary <800048718@fvtc.edu>
2025-09-16 20:51:52 -0400

    Revised the getLength, getWidth & calculateArea functions. Added displayArea function.

--- HC.RectangleArea/Main.cpp
+++ HC.RectangleArea/Main.cpp
@@ -6,46 +6,63 @@
 #include <conio.h> 
 
 using namespace std;
-
-int getLengthFromUser(int length);
-int getWidthFromUser(int width);
+//function prototypes
+int getLengthFromUser();
+int getWidthFromUser();
 int calculateArea(int length, int width);
+void displayArea(int area);
+
 
 int main()
 {
-
-
-    int number = calculateArea(0,0);
-    cout << "The area of the rectangle is: " << number << " cm. \n";
+    //declare variables and call functions
+    int length = getLengthFromUser();
+    int width = getWidthFromUser();
+    int area = calculateArea(length, width);
+    displayArea(area);
+    
 
     (void)_getch();
     return 0;
 }
 
-int getLengthFromUser(int length)
+// get length from user
+int getLengthFromUser()
 {   
-    
+    int length;
     cout << "Enter a positive integer for the length in centimeters: \n";
-    cin >> length; 
+    cin >> length;
 
     return length;
-}
 
-int getWidthFromUser(int width)
+}
+//get width from user
+int getWidthFromUser()
 {
+    int width;
     cout << "Enter a positive integer for the width in centimeters: \n";
     cin >> width;
 
     return width;
+
 }
 
+//calculate area
 int calculateArea(int length, int width)
 {   
-
-    int area = getLengthFromUser(0) * getWidthFromUser(0);
-    
-   return area;
+   return length * width;
 
 }
+
+//display area
+void displayArea(int area)
+{
+
+    cout << "The area of the rectangle is: " << area << " cm. \n";
+
+ 
+}
+
+
 
 
